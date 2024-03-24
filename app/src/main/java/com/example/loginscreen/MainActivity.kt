cca3f9625fd8b0e5f9538f69a24da74cf166421d
rafabcanedo <rafaitecanedo@gmail.com>
2024-03-24 20:04:29 -0300

    log username password function

--- app/src/main/java/com/example/loginscreen/MainActivity.kt
+++ app/src/main/java/com/example/loginscreen/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.loginscreen
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var usernameInput: EditText
+    lateinit var passwordInput: EditText
+    lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,16 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        UsernameInput = findViewById<InputUsername>(R.id.input_username)
+        PasswordInput = findViewById<InputPassword>(R.id.input_password)
+        LoginButton = findViewById<ButtonLogin>(R.id.button_login)
+
+        loginButton.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Log.i("Credentials", "Username: $username and Password: $password")
         }
     }
 }
